ab2432e054ea25ea5b6d930193d636eda1753541
Matt Moore <MattMoore@matts-MBP.askey.com>
2023-03-01 08:59:11 -0500

    adding more styling and components to sidebar

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import SidebarOption from '../SidebarOption/SidebarOption'
 import './Sidebar.css'
+import HomeIcon from '@mui/icons-material/Home'
+import SearchIcon from '@mui/icons-material/Search';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 
 const Sidebar = () => {
   return (
@@ -9,6 +13,13 @@ const Sidebar = () => {
         src='https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg'
         alt=''
       />
+      <SidebarOption title='Home' Icon={HomeIcon} />
+      <SidebarOption title='Search' Icon={SearchIcon} />
+      <SidebarOption title='Your Library' Icon={LibraryMusicIcon} />
+
+      <br />
+      <strong className='sidebarTitle'>PLAYLISTS</strong>
+      <hr />
     </div>
   )
 }
--- src/SidebarOption/SidebarOption.js
+++ src/SidebarOption/SidebarOption.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './SidebarOption.css'
 
+
 const SidebarOption = ({title, Icon}) => {
   return (
     <div className='sidebarOption'>
+      {Icon && <Icon className='sidebarOptionIcon' />}
+      {Icon ? <h4>{title}</h4> : <p>{title}</p>}
       
     </div>
   )
